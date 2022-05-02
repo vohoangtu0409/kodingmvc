6aa2c7cb246468e7b6fc82589a0e9e01f0a5f976
HoangTu <tuvo@pandavietnamsoftware.com>
2022-05-03 06:28:49 +0700

    Fix Blade using standalone

--- src/Application/View/BladeServiceProvider.php
+++ src/Application/View/BladeServiceProvider.php
@@ -3,11 +3,27 @@
 namespace Application\View;
 
 use Illuminate\View\Engines\CompilerEngine;
+use Illuminate\View\Engines\EngineResolver;
 use Illuminate\View\Engines\FileEngine;
 use Illuminate\View\Engines\PhpEngine;
 use Illuminate\View\ViewServiceProvider;
 
 class BladeServiceProvider extends ViewServiceProvider{
+    public function registerEngineResolver()
+    {
+        $this->app->singleton('view.engine.resolver', function () {
+            $resolver = new EngineResolver;
+
+            // Next, we will register the various view engines with the resolver so that the
+            // environment will resolve the engines needed for various views based on the
+            // extension of view file. We call a method for each of the view's engines.
+            foreach (['file', 'php', 'blade'] as $engine) {
+                $this->{'register'.ucfirst($engine).'Engines'}($resolver);
+            }
+
+            return $resolver;
+        });
+    }
     public function registerFileEngines($resolver)
     {
         $resolver->register('file', function () {
